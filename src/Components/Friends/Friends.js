c9c13f3c0859f4aacd57035870959edd98dda1a4
shafik720 <shafikrasel5@gmail.com>
2022-10-30 21:24:01 +0600

    dfgdg

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Friends = () => {
+    const[friends, setFriends] = useState([]);
     return (
         <div>
             <h3>Hello My Friends</h3>
