927123d21555f636ff20daec27d925d2a41dc08c
shafik720 <shafikrasel5@gmail.com>
2022-10-30 21:33:44 +0600

    dfgdg

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Friend = (props) => {
     const{name, id} = props.index;
+    function showUserDetails(){
+        console.log(id);
+    }
     return (
         <div>
             <h3>Name : {name} </h3>
-            
+            <button onClick={showUserDetails}>Show User</button>
         </div>
     );
 };
