c4e3b16336607fa9ef9c8d41e251f266673d4559
shafik720 <shafikrasel5@gmail.com>
2022-10-30 21:18:49 +0600

    dfgdg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Friends from './Components/Friends/Friends';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route  path="/home" element={<Home></Home>}></Route>
+        <Route  path="/about" element={<About></About>}></Route>
+        <Route  path="/contact" element={<Contact></Contact>}></Route>
+        <Route  path="/friends" element={<Friends></Friends>}></Route>
+      </Routes>
     </div>
   );
 }
