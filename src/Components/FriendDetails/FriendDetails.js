47b984f99333091575f24813ba7e9653880b9377
shafik720 <shafikrasel5@gmail.com>
2022-10-30 21:38:41 +0600

    dfgdg

---
+++ src/Components/FriendDetails/FriendDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendDetails = () => {
+    return (
+        <div>
+            <h3>Show Details</h3>
+        </div>
+    );
+};
+
+export default FriendDetails;
