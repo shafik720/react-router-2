b241f28c98e4c5a452810c099e162b5a6d5e4a42
shafik720 <shafikrasel5@gmail.com>
2022-10-30 21:25:25 +0600

    dfgdg

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Friends = () => {
     const[friends, setFriends] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(data=>setFriends(data))
+    },[])
     return (
         <div>
             <h3>Hello My Friends</h3>
