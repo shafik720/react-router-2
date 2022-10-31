ad28534a11b000573f5f5bf8182a7fd245f75ddc
shafik720 <shafikrasel5@gmail.com>
2022-10-31 11:26:33 +0600

    dsrtgret

---
+++ src/Components/PostDetails/PostDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostDetails = () => {
+    return (
+        <div>
+            <h2>Post Details Here ........ </h2>
+        </div>
+    );
+};
+
+export default PostDetails;
--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Posts = () => {
+    let [post, setPost] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res=>res.json())
+        .then(data=>setPost(data))
+    },[])
     return (
         <div>
-            <h2>Hello From posts</h2>
+            <h2>Hello From posts : {post.length} </h2>
         </div>
     );
 };
