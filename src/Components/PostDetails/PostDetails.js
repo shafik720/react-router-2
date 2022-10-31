51ad486cde654d4546fc8f8227d2dd9f1058fae1
shafik720 <shafikrasel5@gmail.com>
2022-10-31 11:35:44 +0600

    dsrtgret

--- src/Components/PostDetails/PostDetails.js
+++ src/Components/PostDetails/PostDetails.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const PostDetails = () => {
+    let param = useParams();
     return (
         <div>
             <h2>Post Details Here ........ </h2>
+            <h1>Post : {param.postId} </h1>
         </div>
     );
 };
